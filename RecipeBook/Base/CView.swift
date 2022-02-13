e191ea667301a25b1c230fef06b59eab2b7fda13
SenKill <serik1404500@gmail.com>
2022-02-13 12:56:46 +0600

    Create custom classes

---
+++ RecipeBook/Base/CView.swift
@@ -0,0 +1,33 @@
+//
+//  CView.swift
+//  RecipeBook
+//
+//  Created by Serik Musaev on 2/13/22.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - CustomView
+class CView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setViews()
+        layoutViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setViews()
+        layoutViews()
+    }
+    
+    func setViews() {
+        backgroundColor = .white
+    }
+    
+    func layoutViews() {
+        
+    }
+}
--- RecipeBook/SceneDelegate.swift
+++ RecipeBook/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowsScene = (scene as? UIWindowScene) else { return }
+        let navigationController = UINavigationController()
+        let mainViewController = HomeViewController()
+        
+        navigationController.pushViewController(mainViewController, animated: true)
+        
+        window = UIWindow(windowScene: windowsScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
