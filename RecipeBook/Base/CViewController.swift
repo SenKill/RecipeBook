f39a838b90bef4971910262cef928c8843245e20
SenKill <serik1404500@gmail.com>
2022-02-13 13:02:58 +0600

    Add custom classes

---
+++ RecipeBook/Base/CViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CViewController.swift
+//  RecipeBook
+//
+//  Created by Serik Musaev on 2/13/22.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - CustomViewController
+
+class CViewController<V: CView>: UIViewController {
+    
+    override func loadView() {
+        view = V()
+    }
+    
+    var customView: V {
+        view as! V
+    }
+}
