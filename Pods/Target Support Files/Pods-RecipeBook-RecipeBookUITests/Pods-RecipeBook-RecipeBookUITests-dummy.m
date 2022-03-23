924daf730d2d737a31083e8fc65638b5fe433b2a
SenKill <serik1404500@gmail.com>
2022-03-23 17:23:58 +0600

    Add .gitignore

---
+++ Pods/Target Support Files/Pods-RecipeBook-RecipeBookUITests/Pods-RecipeBook-RecipeBookUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RecipeBook_RecipeBookUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RecipeBook_RecipeBookUITests
+@end
---
+++ Pods/Target Support Files/Pods-RecipeBook/Pods-RecipeBook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RecipeBook : NSObject
+@end
+@implementation PodsDummy_Pods_RecipeBook
+@end
